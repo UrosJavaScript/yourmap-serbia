4951c91befc477dab223eb7775a287b6992735e5
kowcicuros70@gmail.com <kowcicuros70@gmail.com>
2024-04-12 13:17:05 +0200

    adding new logotip

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,9 +1,7 @@
 import "./style.css";
 import BgImage from "../../assets/bg-map.jpg";
-import Icon1 from "../../assets/HIBI.png";
-import Icon2 from "../../assets/NACIONALNI PARK.png";
-import Icon3 from "../../assets/SALAS.png";
-// import { PulsAnimation } from "../animations/puls";
+
+import { LogotipAbsolute } from "../logotipi";
 
 export const Home = () => {
   return (
@@ -17,36 +15,7 @@ export const Home = () => {
           backgroundSize: "cover",
         }}
       >
-        <a
-          href="https://google.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="icon icon1"
-        >
-          {/* <PulsAnimation> */}
-          <img src={Icon1} alt="Icon1" className="img-icon" />
-          {/* </PulsAnimation> */}
-        </a>
-        <a
-          href="https://google.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="icon icon2"
-        >
-          {/* <PulsAnimation> */}
-          <img src={Icon2} alt="Icon2" className="img-icon" />
-          {/* </PulsAnimation> */}
-        </a>
-        <a
-          href="https://google.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="icon icon3"
-        >
-          {/* <PulsAnimation> */}
-          <img src={Icon3} alt="Icon3" className="img-icon" />
-          {/* </PulsAnimation> */}
-        </a>
+        <LogotipAbsolute />
       </div>
 
       <div>
---
+++ src/components/logotipi/index.jsx
@@ -0,0 +1,185 @@
+import { PulsAnimation } from "../animations/puls";
+// icons
+import Icon1 from "../../assets/logotipi/BENU.png";
+import Icon2 from "../../assets/logotipi/CLUB A.png";
+import Icon3 from "../../assets/logotipi/EXPRESS.png";
+import Icon4 from "../../assets/logotipi/GREY copy.png";
+import Icon6 from "../../assets/logotipi/Grand 6.png";
+import Icon7 from "../../assets/logotipi/Group 33.png";
+import Icon8 from "../../assets/logotipi/HIBI copy 2.png";
+import Icon9 from "../../assets/logotipi/HOP NA KOP.png";
+import Icon10 from "../../assets/logotipi/IDEA copy 2.png";
+import Icon11 from "../../assets/logotipi/INFO KOP.png";
+import Icon12 from "../../assets/logotipi/JAT APARTMANI 2.png";
+import Icon13 from "../../assets/logotipi/KLJUNAC.png";
+import Icon14 from "../../assets/logotipi/KOLIBA.png";
+/*
+import Icon15 from "../../assets/logotipi/KOPAOING TOURS.png";
+import Icon16 from "../../assets/logotipi/Kraljevi Cardaci skijasnica.png";
+import Icon17 from "../../assets/logotipi/Layer 29.png";
+import Icon18 from "../../assets/logotipi/MAGLIC.png";
+import Icon19 from "../../assets/logotipi/MONS.png";
+import Icon20 from "../../assets/logotipi/NACIONALNI PARK.png";
+import Icon21 from "../../assets/logotipi/Nebeska Stolica copy.png";
+import Icon22 from "../../assets/logotipi/Oktagon copy.png";
+import Icon23 from "../../assets/logotipi/Nebeska Stolica.png";
+import Icon24 from "../../assets/logotipi/PAJINO PRESLO.png";
+import Icon25 from "../../assets/logotipi/PLANET BIKE copy 2.png";
+import Icon26 from "../../assets/logotipi/SALAS.png";
+import Icon27 from "../../assets/logotipi/SKI RENT DEPO.png";
+import Icon28 from "../../assets/logotipi/SKIJALISTA SRBIJE.png";
+import Icon29 from "../../assets/logotipi/THE CAMELOT.png";
+import Icon30 from "../../assets/logotipi/TO RASKA copy 2.png";
+import Icon31 from "../../assets/logotipi/TO RASKA.png";
+import Icon32 from "../../assets/logotipi/TO RASKA copy.png";
+import Icon33 from "../../assets/logotipi/VICEROY.png";
+import Icon34 from "../../assets/logotipi/VILA KOPAONIK.png";
+import Icon35 from "../../assets/logotipi/VIVA KOP.png";
+import Icon36 from "../../assets/logotipi/ZUPA.png";*/
+
+export const LogotipAbsolute = () => {
+  return (
+    <>
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon1"
+      >
+        <PulsAnimation>
+          <img src={Icon1} alt="Icon1" className="img-icon" />
+        </PulsAnimation>
+      </a>
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon2"
+      >
+        <PulsAnimation>
+          <img src={Icon2} alt="Icon2" className="img-icon" />
+        </PulsAnimation>
+      </a>
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon3"
+      >
+        <PulsAnimation>
+          <img src={Icon3} alt="Icon3" className="img-icon" />
+        </PulsAnimation>
+      </a>
+
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon4"
+      >
+        <PulsAnimation>
+          <img src={Icon4} alt="Icon4" className="img-icon" />
+        </PulsAnimation>
+      </a>
+
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon6"
+      >
+        <PulsAnimation>
+          <img src={Icon6} alt="Icon6" className="img-icon" />
+        </PulsAnimation>
+      </a>
+
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon7"
+      >
+        <PulsAnimation>
+          <img src={Icon7} alt="Icon7" className="img-icon" />
+        </PulsAnimation>
+      </a>
+
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon8"
+      >
+        <PulsAnimation>
+          <img src={Icon8} alt="Icon8" className="img-icon" />
+        </PulsAnimation>
+      </a>
+
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon9"
+      >
+        <PulsAnimation>
+          <img src={Icon9} alt="Icon9" className="img-icon" />
+        </PulsAnimation>
+      </a>
+
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon10"
+      >
+        <PulsAnimation>
+          <img src={Icon10} alt="Icon10" className="img-icon" />
+        </PulsAnimation>
+      </a>
+
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon11"
+      >
+        <PulsAnimation>
+          <img src={Icon11} alt="Icon11" className="img-icon" />
+        </PulsAnimation>
+      </a>
+
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon12"
+      >
+        <PulsAnimation>
+          <img src={Icon12} alt="Icon12" className="img-icon" />
+        </PulsAnimation>
+      </a>
+
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon13"
+      >
+        <PulsAnimation>
+          <img src={Icon13} alt="Icon13" className="img-icon" />
+        </PulsAnimation>
+      </a>
+
+      <a
+        href="https://google.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="icon icon14"
+      >
+        <PulsAnimation>
+          <img src={Icon14} alt="Icon14" className="img-icon" />
+        </PulsAnimation>
+      </a>
+    </>
+  );
+};
